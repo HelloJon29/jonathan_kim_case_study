49ef36f4109b9fa4ae0d25c7d08d1a6c91735c22
HelloJon29 <jonkim1996@gmail.com>
2022-04-22 14:10:11 -0500

    added lamda expression to runner class to print to console

--- monster_factory/src/main/java/com/jonathankim/monster_factory/MonsterFactoryApplication.java
+++ monster_factory/src/main/java/com/jonathankim/monster_factory/MonsterFactoryApplication.java
@@ -1,7 +1,9 @@
 package com.jonathankim.monster_factory;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -9,6 +11,11 @@ public class MonsterFactoryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonsterFactoryApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> System.out.println("Monster Factory Application Started!");
 	}
 
 }
